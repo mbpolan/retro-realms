462966a30f788d6b0c7af9a7b300b102a56c6283
mbpolan <mbpolan@gmail.com>
2016-01-16 23:50:26 -0500

    * Externalized tile scaling

--- src/main/webapp/scene.js
+++ src/main/webapp/scene.js
@@ -8,33 +8,36 @@ module.directive('scene', ['$log', '$http', function ($log, $http) {
         restrict: 'E',
         scope: {},
         template: '<div></div>',
-        link: function (scope, el, attrs) {
+        link: function (scope, el) {
+            var TILE_SIZE = 16;
+            var TILE_SCALE = 2;
+            var TILES_WIDE = 15;
+            var TILES_HIGH = 15;
+
             var renderer = PIXI.autoDetectRenderer();
+            var stage = new PIXI.Container();
             el.find('div')[0].appendChild(renderer.view);
 
             var texturesLoaded = function () {
                 $log.info('Scene initialized');
 
-                var tileSize = 32;
-                var tilesWide = 10;
-                var tilesHigh = 10;
-
-                for (var x = 0; x < tilesWide; x++) {
-                    for (var y = 0; y < tilesHigh; y++) {
-                        placeTile('grass', x * tileSize, y * tileSize);
+                for (var x = 0; x < TILES_WIDE; x++) {
+                    for (var y = 0; y < TILES_HIGH; y++) {
+                        placeTile('0', x * TILE_SIZE, y * TILE_SIZE);
                     }
                 }
+
+                gameLoop();
             };
 
             var placeTile = function (id, x, y) {
                 var tile = PIXI.Sprite.fromFrame(id);
                 tile.x = x;
                 tile.y = y;
-                tile.scale.x = 2;
-                tile.scale.y = 2;
+                tile.scale.x = TILE_SCALE;
+                tile.scale.y = TILE_SCALE;
 
                 stage.addChild(tile);
-                renderer.render(stage);
             };
 
             var loadTextureMap = function (file, callback) {
@@ -44,13 +47,14 @@ module.directive('scene', ['$log', '$http', function ($log, $http) {
                 PIXI.loader
                     .add(image)
                     .load(function () {
-                        var baseImage = PIXI.TextureCache[image];
+                        var baseImage = PIXI.utils.TextureCache[image];
 
                         $http.get(baseUrl + '.json').then(function (result) {
                             for (var key in result.data) {
                                 if (result.data.hasOwnProperty(key)) {
                                     var tile = result.data[key];
-                                    loadTile(key, baseImage, new PIXI.Rectangle(tile.x, tile.y, tile.w, tile.h));
+                                    loadTile(tile.id.toString(), baseImage,
+                                        new PIXI.Rectangle(tile.x, tile.y, tile.w, tile.h));
                                 }
                             }
 
@@ -68,9 +72,13 @@ module.directive('scene', ['$log', '$http', function ($log, $http) {
                 PIXI.Texture.addTextureToCache(texture, alias);
             };
 
-            var stage = new PIXI.Container();
-            renderer.render(stage);
+            var gameLoop = function () {
+                requestAnimationFrame(gameLoop);
 
+                renderer.render(stage);
+            };
+
+            renderer.render(stage);
             loadTextureMap('hyrule_tileset', texturesLoaded);
         }
     }
